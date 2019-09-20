e15befb5f40befb0e55b4d1dfc6d02498b843708
SLem <slemjet@gmail.com>
2019-09-20 15:32:12 +0200

    React project - 2 way binding

--- src/App.js
+++ src/App.js
@@ -3,17 +3,49 @@ import './App.css';
 import Person from "./Person/Person";
 
 class App extends Component {
+    state = {
+        persons: [
+            {name: "Sergei", age: 34, job: "Dev", hobbies: "Games, Study, Gym"},
+            {name: "Lena", age: 31, job: "QA", hobbies: "Gym, Reading"}
+        ]
+    }
+
+    switchNameHandler = (newName) => {
+        let persons = this.state.persons;
+        persons[0].name = newName;
+        this.setState(persons);
+    }
+
+    nameChangeHandler = (event) =>{
+        let persons = this.state.persons;
+        persons[1].name = event.target.value;
+        this.setState(persons);
+    }
+
     render() {
         return (
             <div className="App">
                 <div>
-                    <Person name="Sergei" age='34' job = 'Dev'/>
-                    <Person name="Lena" age='31' job = 'QA'/>
+                    <button onClick={this.switchNameHandler.bind(this, "another name")}>Press me!</button>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}
+                        job={this.state.persons[0].job}
+                        click={this.switchNameHandler.bind(this, "My new name! ")}>
+                        changed={this.nameChangeHandler}
+                        {this.state.persons[0].hobbies}
+                    </Person>
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[0].age}
+                        job={this.state.persons[1].job}
+                        changed={this.nameChangeHandler}>
+                        {this.state.persons[0].hobbies}
+                    </Person>
                 </div>
             </div>
         );
     }
-
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "./Person.css"
 
 const person = (props) => {
     return (
-        <div class="person">
-            <h1>My name is {props.name}</h1>
+        <div className="Person">
+            <h1 onClick={props.click}>My name is {props.name}</h1>
+            <input type="text" onChange={props.changed} value={props.name}/>
             <p>I am {props.age} years old</p>
             <p>I am a {props.job}</p>
             <p>My salary is ${Math.floor(Math.random() * 100000) + 50000}</p>
+            <p>My hobbies are: {props.children}</p>
         </div>
     )
 }
